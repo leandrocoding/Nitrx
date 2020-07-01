1ab164548b723079ca8561c5831aaf626574c081
LAZ13 <leandro@zazzi.eu>
2020-07-01 23:18:48 +0200

    Added Logging file

--- Nitrx.py
+++ Nitrx.py
@@ -12,6 +12,8 @@ import json
 import time
 import re
 import os
+import logging
+
 
 def config():
     try:
@@ -41,16 +43,20 @@ header = str("\033[95m")
 red = str("\033[1;31m")
 bold = str("\033[;1m")
 not_checked = []
+LOG_FILENAME = 'Log.log'
+logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+
 
 def headers():
     header = {
-        'User-Agent' : ua.random,
-        'content-type':'application/json',
+        'User-Agent': ua.random,
+        'content-type': 'application/json',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
         'Connection': 'keep-alive',
     }
     return header
+
 
 def process():
     print("    [+] Process Running Successfully... \n")
@@ -61,6 +67,7 @@ def process():
         if str(sec).endswith("00"):
             os.system("clear")
 
+
 def proxies():
     line = random.choice(proxy_text)
     ip = line.replace('\n', '')
@@ -70,10 +77,11 @@ def proxies():
         https = "https://"+ip
         http = "http://"+ip
     proxy = {
-        "https":https,
-        "http":http
+        "https": https,
+        "http": http
     }
     return proxy
+
 
 def code():
     if option == 2:
@@ -82,16 +90,20 @@ def code():
             code = line.replace('\n', '')
             codes.remove(line)
         except IndexError:
-            code = ('').join(random.choices(string.ascii_letters + string.digits, k=16))
+            code = ('').join(random.choices(
+                string.ascii_letters + string.digits, k=16))
     else:
-        code = ('').join(random.choices(string.ascii_letters + string.digits, k=16))
+        code = ('').join(random.choices(
+            string.ascii_letters + string.digits, k=16))
     return code
+
 
 def save(code):
     file = open(config['output'], 'a')
     write_code = code + "\n"
     file.write(write_code)
     file.close()
+
 
 def debug(code, text, proxy, show):
     try:
@@ -113,6 +125,9 @@ def debug(code, text, proxy, show):
     logo = str(blue + "[" + red + "nitrx" + blue + "]" + reset)
     print('{0:16} {1:26} {2:18} {3:22}'.format(logo, code, text, line))
 
+    logging.debug('{0:16} {1:26} {2:18} {3:22}'.format(logo, code, text, line))
+
+
 def nitrx(code, headers, proxy):
     try:
         global running
@@ -121,7 +136,8 @@ def nitrx(code, headers, proxy):
         running = 0
     s = requests.session()
     s.proxies = proxy
-    url = "https://discordapp.com/api/v6/entitlements/gift-codes/{}?with_application=false&with_subscription_plan=true".format(code)
+    url = "https://discordapp.com/api/v6/entitlements/gift-codes/{}?with_application=false&with_subscription_plan=true".format(
+        code)
     try:
         rr = s.get(url, headers=headers, timeout=config['timeout'])
         if "subscription_plan".lower() in (rr.text).lower():
@@ -162,6 +178,7 @@ def nitrx(code, headers, proxy):
     running -= 1
     exit()
 
+
 if __name__ == '__main__':
     try:
         msg = f""" {header}
@@ -201,7 +218,8 @@ if __name__ == '__main__':
             else:
                 time.sleep(1)
                 for unchecked_code in not_checked:
-                    x = Thread(target=nitrx, args=(code(), headers(), proxies(),))
+                    x = Thread(target=nitrx, args=(
+                        code(), headers(), proxies(),))
                     mythreads.append(x)
                     x.start()
                     not_checked.remove(unchecked_code)
